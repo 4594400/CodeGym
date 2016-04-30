9a971d50047a6ccec8491e425bbd729a73b2aeef
4594400 <4594400@gmail.com>
2016-04-30 15:43:40 +0300

    First test commit

---
+++ src/main/java/ua/in/codegym/lesson_001/SumDigits.java
@@ -0,0 +1,20 @@
+package ua.in.codegym.lesson_001;
+
+
+public class SumDigits {
+    public static void main(String[] args) {
+        System.out.println(sum(-256));
+    }
+
+    public static int sum(int number) {
+        char [] chars = String.valueOf(number).toCharArray();
+        int sum=0;
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isDigit(chars[i])) {
+                int digit = Character.getNumericValue(chars[i]);
+                sum +=digit;
+            }
+        }
+        return sum;
+    }
+}
