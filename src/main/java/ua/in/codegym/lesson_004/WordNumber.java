bbb4e2486af1d499c2320124ced56b38eb992135
4594400 <4594400@gmail.com>
2016-05-20 16:00:38 +0300

    Added task 4.2 - WordNumber  (lesson 004)

---
+++ src/main/java/ua/in/codegym/lesson_004/WordNumber.java
@@ -0,0 +1,35 @@
+package ua.in.codegym.lesson_004;
+
+
+import java.util.*;
+
+public class WordNumber {
+    public static void main(String[] args) {
+        System.out.println(count("I would go"));
+
+    }
+
+    public static boolean isLetter(char c){
+        return (c>='a' && c<='z') || (c>='A' && c<='Z');
+    }
+
+    public static int count(String input) {
+        int count=0;
+        String currentWord = "";
+
+        if (input.length() == 0) return 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (isLetter(input.charAt(i))) {
+                currentWord += input.charAt(i);
+            } else if (currentWord.length() > 0) {
+                count++;
+                currentWord="";
+            }
+        }
+        if (currentWord.length()>0) {
+            count++;
+        }
+        return count;
+    }
+}
